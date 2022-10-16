f290049ec116b34fb5ad8240e2ffad34a249a103
Pinto Manuel <pinto@fireworkx.co.za>
2022-10-16 15:36:04 +0200

    Ojects created

---
+++ Js/script.js
@@ -0,0 +1,127 @@
+/* Apartment types */
+const apartmentType = [
+    {
+        id: 0,
+        name: "Apartment",
+        area: "Parkland",
+        size: "2.5 Km",
+        price: "2 million",
+        description: "Is a well located apartment that stays close to a very luxury area",
+
+        apartmentCompartment: [
+            {
+                id: 0,
+                rooms: "1",
+                images: "/assets/icon.png",
+            }
+        ]
+
+    },
+
+    {
+        id: 1,
+        name: "Apartment",
+        area: "Milnerton",
+        size: "4.5 Km",
+        price: "2 million",
+        description: "If you like to live in a area close to a vibrant night life, here is the right spot",
+
+        apartmentCompartment: [
+            {
+                id: 1,
+                rooms: "2",
+                images: "/assets/icon.png",
+            }
+        ]
+    },
+    {
+        id: 2,
+        name: "Apartment",
+        area: "Durban Ville",
+        size: "2.5 Km",
+        price: "5 million",
+        description: "Across the sea side, this is a perfect area for your family to live",
+
+        apartmentCompartment: [
+            {
+                id: 2,
+                rooms: "3",
+                images: "/assets/icon.png",
+            }
+        ]
+    },
+    {
+        id: 3,
+        name: "Apartment",
+        area: "Cape Town - CBD",
+        size: "1.5 Km",
+        price: "3 million",
+        description: "Living in the heal is your thing? Then come and grab this vastiuous luxury apartment with breath taking sight see",
+
+        apartmentCompartment: [
+            {
+                id: 3,
+                images: "/assets/icon.png",
+            }
+        ]
+    },
+    {
+        id: 4,
+        name: "Apartment",
+        area: "Parkland",
+        size: "2.5 Km",
+        price: "2 million",
+        description: "A well spacious 2 bedroom apartiment close to a new developed urban infrastructure",
+
+        apartmentCompartment: [
+            {
+                id: 4,
+                images: "/assets/icon.png",
+            }
+        ]
+    }
+];
+
+
+/* Types of infrastructure */
+const houseTypes = [
+    {
+        id: 0,
+        typeofHouse: "apartment",
+    },
+    {
+        id: 1,
+        typeofHouse: "Single bedroom",
+    },
+    {
+        id: 2,
+        typeofHouse: "double bedroom apartment",
+    },
+    {
+        id: 3,
+        typeofHouse: "House",
+    }
+]
+
+const apartTypes = document.getElementsByClassName('apart_types')[0]
+console.log("Apartment", apartTypes);
+
+
+houseTypes.forEach((product, index) => {
+    let itemP = document.createElement('a');
+    let itemText = document.createTextNode(product.typeofHouse);
+    itemP.appendChild(itemText); 
+    apartTypes.appendChild(itemP)
+})
+
+
+
+
+apartmentType.forEach(element => {
+    console.log(element);
+});
+
+/* 
+houseTypes.forEach(el => {
+    console.log("Apartment type", el);
+}) */
