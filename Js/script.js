68d837b7e442773380b40914d4a7f2ae474e1f03
Pinto Manuel <pinto@fireworkx.co.za>
2022-10-19 20:34:58 +0200

    commit code

--- Js/script.js
+++ Js/script.js
@@ -144,6 +144,25 @@ close.addEventListener('click', () => {
 })
 
 
+/* Search modal */
+const btnSearchOpen = document.getElementById('btn-search-open');
+const megaSearchContainer = document.getElementById('mega_search_container');
+const searchClose = document.getElementById('close_search');
+
+console.log("btn Open", btnSearchOpen);
+console.log("search container", megaSearchContainer);
+console.log("search close", searchClose);
+
+btnSearchOpen.addEventListener('click', () => {
+    megaSearchContainer.classList.add('show');
+})
+
+searchClose.addEventListener('click', () => {
+    megaSearchContainer.classList.remove('show')
+})
+/* End Search modal */
+
+
 /* Sub-links */
 const about_links = [
     {
