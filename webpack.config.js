1240b389463697cba2c50192915bf7257cbb1f30
Pinto Manuel <pinto@fireworkx.co.za>
2022-11-20 13:35:26 +0200

    Commit code

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require("path");
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+
+    /* loaders */
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                /* Note: Order is from last to first */
+                use: [
+                    'style-loader',
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            importLoaders: 1,
+                        }
+                    },
+                    'postcss-loader'
+                ]
+            }
+        ]
+    },
+    devServer: {
+        static: path.resolve(__dirname, 'dist'),
+        port: 9000,
+        open: true
+    }
+};
